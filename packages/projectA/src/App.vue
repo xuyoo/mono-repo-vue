969826116bbb913f55cd780ae3ca524a042a500c
xuyuanyuan52 <xuyuanyuan52@jd.com>
2023-06-02 10:27:01 +0800

    feat: 版本修改

--- packages/projectA/src/App.vue
+++ packages/projectA/src/App.vue
@@ -9,7 +9,7 @@ import QtCard from '@mono-repo-demo/common/components/QtCard.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <qt-card></qt-card>
+      <!-- <qt-card></qt-card> -->
       <!-- <HelloWorld msg="You did it!" /> -->
 
       <nav>
